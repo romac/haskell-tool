8a730b717df646a2f622f74135845070cc54d43b
Romain Ruetschi <romain.ruetschi@gmail.com>
2014-10-21 00:10:26 +0200

    Parse all kinds of expression (had to left-factor the grammar).

--- src/Language/Tool/Parser.hs
+++ src/Language/Tool/Parser.hs
@@ -110,155 +110,241 @@ newtype Ident =
 
 toolDef :: LanguageDef st
 toolDef =
-  javaStyle { nestedComments  = False
-            , reservedNames   = [ "object", "class", "def", "var"
+  javaStyle { T.nestedComments  = False
+            , T.reservedNames   = [ "object", "class", "def", "var"
                                 , "Unit", "main", "String", "extends"
                                 , "Int", "Bool", "while", "if", "else"
                                 , "return", "length", "true", "false"
                                 , "this", "new", "println" ]
-            , reservedOpNames = [ "+", "-", "*", "/"
+            , T.reservedOpNames = [ "+", "-", "*", "/"
                                 , "<", "||", "&&", "!" ]
-            , caseSensitive   = True
+            , T.caseSensitive   = True
             }
 
-lexer = Token.makeTokenParser toolDef
+lexer = T.makeTokenParser toolDef
 
-identifier = Token.identifier lexer
-reserved   = Token.reserved   lexer
-reservedOp = Token.reservedOp lexer
-parens     = Token.parens     lexer
-integer    = Token.integer    lexer
-semi       = Token.semi       lexer
-colon      = Token.semi       lexer
-whiteSpace = Token.whiteSpace lexer
-braces     = Token.braces     lexer
-brackets   = Token.brackets   lexer
-symbol     = Token.symbol     lexer
+identifier    = T.identifier    lexer
+reserved      = T.reserved      lexer
+-- reservedOp = T.reservedOp    lexer
+parens        = T.parens        lexer
+integer       = T.integer       lexer
+stringLiteral = T.stringLiteral lexer
+semi          = T.semi          lexer
+colon         = T.colon         lexer
+whiteSpace    = T.whiteSpace    lexer
+braces        = T.braces        lexer
+brackets      = T.brackets      lexer
+symbol        = T.symbol        lexer
+dot           = T.dot           lexer
 
-parseTool :: Parser Program
-parseTool = whiteSpace >> program
+comma         = symbol ","
 
-program :: Parser Program
-program = do
-  main    <- mainObject
-  classes <- many classDecl
+empty :: Parser ()
+empty = return ()
+
+lit :: String -> a -> Parser a
+lit s t = reserved s >> return t
+
+topLevel :: Parser a -> Parser a
+topLevel = between whiteSpace eof
+
+toolParser :: Parser Program
+toolParser = topLevel parseProgram
+
+parseProgram :: Parser Program
+parseProgram = do
+  main    <- parseMainObject
+  classes <- many parseClassDecl
   return $ Program main classes
 
-mainObject :: Parser MainObject
-mainObject = do
+parseMainObject :: Parser MainObject
+parseMainObject = do
   reserved "object"
-  id    <- ident
+  id    <- parseIdent
   stmts <- braces (do
     reserved "def"
     reserved "main"
+    parens empty
     colon
     reserved "Unit"
     symbol "="
-    braces (many stmt))
+    braces (many parseStmt))
   return $ MainObject id stmts
 
-classDecl :: Parser ClassDecl
-classDecl = do
+parseClassDecl :: Parser ClassDecl
+parseClassDecl = do
   reserved "class"
-  id     <- ident
-  parent <- optionMaybe (reserved "extends" >> ident)
-  vars   <- many varDecl
-  meths  <- many methodDecl
+  id     <- parseIdent
+  parent <- optionMaybe (reserved "extends" >> parseIdent)
+  symbol "{"
+  vars   <- many parseVarDecl
+  meths  <- many parseMethodDecl
+  symbol "}"
   return $ ClassDecl id parent vars meths
 
-varDecl :: Parser VarDecl
-varDecl = do
+parseVarDecl :: Parser VarDecl
+parseVarDecl = do
   reserved "var"
-  id <- ident
+  id <- parseIdent
   colon
   tpe <- parseType
   semi
   return $ VarDecl id tpe
 
-methodDecl :: Parser MethodDecl
-methodDecl = do
+parseMethodDecl :: Parser MethodDecl
+parseMethodDecl = do
   reserved "def"
-  name <- ident
-  args <- parens $ formal `sepBy` symbol ","
+  name <- parseIdent
+  args <- parens $ parseFormal `sepBy` comma
   colon
   retType <- parseType
   symbol "="
   symbol "{"
-  vars <- many varDecl
-  stmts <- many stmt
+  vars <- many parseVarDecl
+  stmts <- many parseStmt
   reserved "return"
-  retExpr <- expr
+  retExpr <- parseExpr
   semi
   symbol "}"
   return $ MethodDecl name retType args vars stmts retExpr
 
-formal :: Parser Formal
-formal = do
-  id <- ident
+parseFormal :: Parser Formal
+parseFormal = do
+  id <- parseIdent
   colon
   tpe <- parseType
   return $ Formal id tpe
 
 parseType :: Parser Type
-parseType =  (reserved "Int" >> symbol "[" >> symbol "]" >> return IntArrayType)
-         <|> (reserved "Int" >> return IntType)
+parseType =  parseIntType
          <|> (reserved "Bool" >> return BoolType)
          <|> (reserved "String" >> return StringType)
-         <|> liftM ClassType ident
+         <|> ClassType <$> parseIdent
 
-stmt :: Parser Stmt
-stmt =  parseBlock
+parseIntType :: Parser Type
+parseIntType = do
+  reserved "Int"
+  (brackets empty >> return IntArrayType) <|> (return IntType)
+
+parseStmt :: Parser Stmt
+parseStmt =  parseBlock
     <|> parseIf
-    -- <|> parseWhile
+    <|> parseWhile
     <|> parsePrintln
-    <|> parseAssign
-    <|> parseArrayAssign
+    <|> try parseAssign
+    <|> try parseArrayAssign
 
 parseBlock :: Parser Stmt
 parseBlock = do
-  stmt <- braces (many stmt)
+  stmt <- braces (many parseStmt)
   return $ Block stmt
 
 parseIf :: Parser Stmt
 parseIf = do
   reserved "if"
-  cond <- parens expr
-  thn <- stmt
-  els <- optionMaybe (reserved "else" >> stmt)
+  cond <- parens parseExpr
+  thn <- parseStmt
+  els <- optionMaybe (reserved "else" >> parseStmt)
   return $ If cond thn els
+
+parseWhile :: Parser Stmt
+parseWhile = do
+  reserved "while"
+  cond <- parens parseExpr
+  stmt <- parseStmt
+  return $ While cond stmt
 
 parsePrintln :: Parser Stmt
 parsePrintln = do
   reserved "println"
-  t <- parens expr
+  t <- parens parseExpr
   semi
   return $ Println t
 
 parseAssign :: Parser Stmt
 parseAssign = do
-  id <- ident
+  id <- parseIdent
   symbol "="
-  val <- expr
+  val <- parseExpr
   semi
   return $ Assign id val
 
 parseArrayAssign :: Parser Stmt
 parseArrayAssign = do
-  id <- ident
-  idx <- brackets expr
+  id <- parseIdent
+  idx <- brackets parseExpr
   symbol "="
-  val <- expr
+  val <- parseExpr
   semi
   return $ ArrayAssign id idx val
 
-expr :: Parser Expr
-expr =  (reserved "true"  >> return TrueLit)
-    <|> (reserved "false" >> return FalseLit)
-    <|> (reserved "this"  >> return This)
-    <|> parens expr
-    <|> liftM Identifier ident
+parseExpr :: Parser Expr
+parseExpr = parseTerm >>= parseExprRest
 
-ident :: Parser Ident
-ident = liftM Ident identifier
+parseExprRest :: Expr -> Parser Expr
+parseExprRest t =
+      try (parseArrayRead t)
+  <|> try (parseArrayLength t)
+  <|> try (parseMethodCall t)
+  <|> return t
 
+parseTerm :: Parser Expr
+parseTerm =
+      (lit "true"  TrueLit)
+  <|> (lit "false" FalseLit)
+  <|> (lit "this"  This)
+  <|> IntLit     <$> integer
+  <|> StringLit  <$> stringLiteral
+  <|> Identifier <$> parseIdent
+  <|> try parseNewIntArray
+  <|> try parseNew
+  <|> (symbol "!" >> Not <$> parseExpr)
+  <|> parens parseExpr
 
+parseArrayRead :: Expr -> Parser Expr
+parseArrayRead arr = do
+  idx <- brackets parseExpr
+  parseExprRest $ ArrayRead arr idx
+
+parseArrayLength :: Expr -> Parser Expr
+parseArrayLength arr = do
+  dot
+  reserved "length"
+  parseExprRest $ ArrayLength arr
+
+parseMethodCall :: Expr -> Parser Expr
+parseMethodCall obj = do
+  dot
+  meth <- parseIdent
+  args <- parens (parseExpr `sepBy` comma)
+  parseExprRest $ MethodCall obj meth args
+
+parseNew :: Parser Expr
+parseNew = do
+  symbol "new"
+  id <- parseIdent
+  parens empty
+  return $ New id
+
+parseNewIntArray :: Parser Expr
+parseNewIntArray = do
+  symbol "new"
+  reserved "Int"
+  size <- brackets parseExpr
+  return $ NewIntArray size
+
+parseIdent :: Parser Ident
+parseIdent = Ident <$> identifier
+
+parseString :: String -> Program
+parseString str =
+  case parse toolParser "" str of
+    Left e  -> error $ show e
+    Right r -> r
+
+parseFile :: String -> IO Program
+parseFile file =
+  do program  <- readFile file
+     case parse toolParser "" program of
+       Left e  -> print e >> fail "parse error"
+       Right r -> return r
